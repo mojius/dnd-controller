77c8264f99ef570c2c683012bf2d7dd3d9225e37
Ben Daly <56139197+mojius@users.noreply.github.com>
2024-04-20 20:31:31 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,7 @@ function DexCheck () {
     }
 }
 input.onButtonPressed(Button.A, function () {
+    mode = modeSelector
     if (mode == 1) {
         Score_1 += 1
     }
@@ -32,6 +33,40 @@ function ModeLEDSelect () {
             . # . # .
             . # # . .
             `)
+    } else if (modeSelector == 3) {
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            . # # # .
+            `)
+    } else if (modeSelector == 4) {
+        basic.showLeds(`
+            . . # # .
+            . # . . .
+            . # . . .
+            . # . . .
+            . . # # .
+            `)
+    } else if (modeSelector == 5) {
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+    } else if (modeSelector == 6) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # # # .
+            . # . . .
+            . # . . .
+            `)
+    } else {
+    	
     }
 }
 function StrengthCheck () {
@@ -61,15 +96,12 @@ function StrengthCheck () {
             `)
     }
 }
-input.onButtonPressed(Button.AB, function () {
-    mode = modeSelector
-})
 input.onButtonPressed(Button.B, function () {
     if (mode == 1) {
         Score_2 += 1
     }
     if (mode == 0) {
-        modeSelector += -1
+        modeSelector += 1
     }
 })
 let Score_2 = 0
@@ -79,7 +111,7 @@ let modeSelector = 0
 let mode = 0
 mode = 0
 modeSelector = 1
-let modeCount = 2
+let modeCount = 6
 basic.forever(function () {
     if (mode == 0) {
         if (modeSelector > modeCount) {
@@ -96,9 +128,11 @@ basic.forever(function () {
         } else if (mode == 3) {
             music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
         } else if (mode == 4) {
-            music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+            music.play(music.createSoundExpression(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
         } else if (mode == 5) {
-        	
+            music.play(music.createSoundExpression(WaveShape.Noise, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+        } else if (mode == 6) {
+            music.play(music.createSoundExpression(WaveShape.Sawtooth, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
         } else {
         	
         }
